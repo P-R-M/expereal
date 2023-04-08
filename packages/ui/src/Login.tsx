712f82e1698228de64368008488f380d1f56428f
lucienmakutano <lucien.programmer@gmail.com>
2023-04-09 00:25:46 +0300

    Login ui component

--- packages/ui/src/Login.tsx
+++ packages/ui/src/Login.tsx
@@ -1,20 +1,11 @@
 import React from "react";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-// import { useHistory } from 'react-router-dom'; // if you use react-router
-// import GoogleButton from 'react-google-button' // optional
 
-interface LoginProps {
-  firebase: any;
-  auth: any;
-  isLoaded: any;
-  isEmpty: any;
-  navigate: any;
+type LoginProps = {
+  firebase: any
 }
 
-export const LoginPage = (props: LoginProps) => {
-  //   const firebase = useFirebase();
-  //   const auth = useSelector((state) => state.firebase.auth);
-  const { firebase, auth, isLoaded, isEmpty, navigate } = props;
+export const LoginComp = ({  firebase }: LoginProps) => {
 
   return (
     <div>
@@ -24,26 +15,16 @@ export const LoginPage = (props: LoginProps) => {
           signInSuccessUrl: "/signedIn",
           signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
           callbacks: {
-            signInSuccessWithAuthResult: (authResult, redirectUrl) => {
-              firebase.handleRedirectResult(authResult).then(() => {
-                navigate(redirectUrl); // if you use react router to redirect
-              });
+            signInSuccessWithAuthResult: (_authResult, _redirectUrl) => {
+              // firebase.handleRedirectResult(authResult).then(() => {
+              //   // history.push(redirectUrl); if you use react router to redirect
+              // });
               return false;
             },
           },
         }}
         firebaseAuth={firebase.auth()}
       />
-      <div>
-        <h2>Auth</h2>
-        {!isLoaded(auth) ? (
-          <span>Loading...</span>
-        ) : isEmpty(auth) ? (
-          <span>Not Authed</span>
-        ) : (
-          <pre>{JSON.stringify(auth, null, 2)}</pre>
-        )}
-      </div>
     </div>
   );
 }
