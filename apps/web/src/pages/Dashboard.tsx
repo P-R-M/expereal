cf6c566afa80a67f417e2fd22cc01b30dc908953
lucienmakutano <lucien.programmer@gmail.com>
2023-04-10 16:22:40 +0300

    Separate concerns in components

--- apps/web/src/pages/Dashboard.tsx
+++ apps/web/src/pages/Dashboard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ChatForm from "../components/ChatForm";
 import ChatRoom from "../components/ChatRoom";
 import Recipients from "../components/Recipients";
+import ChatProfile from "../components/ChatProfile";
 
 // https://tailwindcomponents.com/component/quickchat-chat-layout
 
@@ -35,23 +36,7 @@ function Dashboard() {
             </div>
             <div className="ml-2 font-bold text-2xl">ExpChat</div>
           </div>
-          <div className="flex flex-col items-center bg-indigo-100 border border-gray-200 mt-4 w-full py-6 px-4 rounded-lg">
-            <div className="h-20 w-20 rounded-full border overflow-hidden">
-              <img
-                src="https://avatars3.githubusercontent.com/u/2763884?s=128"
-                alt="Avatar"
-                className="h-full w-full"
-              />
-            </div>
-            <div className="text-sm font-semibold mt-2">Aminos Co.</div>
-            <div className="text-xs text-gray-500">Advisor - PhD Computer Science</div>
-            <div className="flex flex-row items-center mt-3">
-              <div className="flex flex-col justify-center h-4 w-8 bg-indigo-500 rounded-full">
-                <div className="h-3 w-3 bg-white rounded-full self-end mr-1"></div>
-              </div>
-              <div className="leading-none ml-1 text-xs">Active</div>
-            </div>
-          </div>
+          <ChatProfile />
           <Recipients />
         </div>
         <div className="flex flex-col flex-auto p-6">
